22a6bba7f24a840ac0d8a105791b3dd0209246ae
Muhammad Saqib Ijaz <muhammadsaqibijazkhan@gmail.com>
2026-08-25 17:22:30 +0500

    Testing the Translator Module

---
+++ services/test_translator.py
@@ -0,0 +1,10 @@
+from translator import translate_to_urdu , translate_to_english
+
+# text = "Hello, how are you?"
+# translator = translate_to_urdu(text)
+# print(f"Translated text: {translator}")
+
+text_in_urdu = "ہیلو، آپ کیسے ہیں؟" 
+translator = translate_to_english(text_in_urdu)
+print(f"Translated text: {translator}")
+
